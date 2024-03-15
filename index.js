9c20133967205638fe1501e479a12d37d37af843
Valentyn Yurchenko <valentin.urchenko@gmail.com>
2024-03-15 17:47:20 +0200

    add alias data

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const findPage = require('./data/findPage.json');
-const pageByAlias = require('./data/pageByAlias.json');
-const findProduct = require('./data/findProduct.json');
+const express = require("express");
+const cors = require("cors");
+const findPage = require("./data/findPage.json");
+const pageByAliasPhotoshop = require("./data/pageByAliasPhotoshop.json");
+const pageByAliasFinancialAnalitics = require("./data/pageByAliasFinancialAnalytics.json");
+const findProduct = require("./data/findProduct.json");
 
 const app = express();
 
 app.use(cors());
 
-app.get('/api/top-page/find', (req, res) => {
+app.get("/api/top-page/find", (req, res) => {
   res.json(findPage);
 });
 
-app.get('/api/top-page/byAlias/photoshop', (req, res) => {
-  res.json(pageByAlias);
+app.get("/api/top-page/byAlias/photoshop", (req, res) => {
+  res.json(pageByAliasPhotoshop);
 });
 
-app.get('/api/product/find', (req, res) => {
+app.get("/api/top-page/byAlias/financial-analytics", (req, res) => {
+  res.json(pageByAliasFinancialAnalitics);
+});
+
+app.get("/api/product/find", (req, res) => {
   res.json(findProduct);
 });
 
