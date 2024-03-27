07331f8484412534bf8c2743b6dea69419ab2601
Valentyn Yurchenko <valentin.urchenko@gmail.com>
2024-03-27 17:25:38 +0200

    Added new pages by alias

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-const express = require("express");
-const cors = require("cors");
-const findPage = require("./data/findPage.json");
-const pageByAliasPhotoshop = require("./data/pageByAliasPhotoshop.json");
-const pageByAliasFinancialAnalitics = require("./data/pageByAliasFinancialAnalytics.json");
-const findProduct = require("./data/findProduct.json");
+const express = require('express');
+const cors = require('cors');
+const findPage = require('./data/findPage.json');
+const pageByAliasPhotoshop = require('./data/pageByAliasPhotoshop.json');
+const pageByAliasFinancialAnalitics = require('./data/pageByAliasFinancialAnalytics.json');
+const pageByAliasPhography = require('./data/pageByAliasPhotography.json');
+const pageByAliasBridge = require('./data/pageByAliasBridge.json');
+const pageByAliasInventor = require('./data/pageByAliasInventor.json');
+const pageByAliasPython = require('./data/pageByAliasPython.json');
+const pageByAliasTrade = require('./data/pageByAliasTrade.json');
+const findProduct = require('./data/findProduct.json');
 
 const app = express();
 
 app.use(cors());
 
-app.get("/api/top-page/find", (req, res) => {
+app.get('/api/top-page/find', (req, res) => {
   res.json(findPage);
 });
 
-app.get("/api/top-page/byAlias/photoshop", (req, res) => {
+app.get('/api/top-page/byAlias/photoshop', (req, res) => {
   res.json(pageByAliasPhotoshop);
 });
 
-app.get("/api/top-page/byAlias/financial-analytics", (req, res) => {
+app.get('/api/top-page/byAlias/financial-analytics', (req, res) => {
   res.json(pageByAliasFinancialAnalitics);
 });
 
-app.get("/api/product/find", (req, res) => {
+app.get('/api/top-page/byAlias/photography', (req, res) => {
+  res.json(pageByAliasPhography);
+});
+
+app.get('/api/top-page/byAlias/bridge', (req, res) => {
+  res.json(pageByAliasBridge);
+});
+
+app.get('/api/top-page/byAlias/inventor', (req, res) => {
+  res.json(pageByAliasInventor);
+});
+
+app.get('/api/top-page/byAlias/python', (req, res) => {
+  res.json(pageByAliasPython);
+});
+
+app.get('/api/top-page/byAlias/trade', (req, res) => {
+  res.json(pageByAliasTrade);
+});
+
+app.get('/api/product/find', (req, res) => {
   res.json(findProduct);
 });
 
